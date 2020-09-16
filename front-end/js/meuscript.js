43f1c6b526858e17a0b45197fe0643de71b50ae2
Vanessa <vanessadesouza020403@gmail.com>
2020-09-16 20:58:31 -0300

    adição da função incluir

--- front-end/js/meuscript.js
+++ front-end/js/meuscript.js
@@ -50,7 +50,55 @@ $(function() { // quando o documento estiver pronto/carregado
     $(document).on("click", "#linkInicio", function() {
         mostrar_conteudo("conteudoInicial");
     });
+// código para mapear click do botão incluir profissao
+    $(document).on("click", "#btIncluirMusica", function() {
+        //pegar dados da tela
+        nome = $("#campoNome").val();
+        artista = $("#campoArtista").val();
+        genero = $("#campoGenero").val();
+        ano = $("#campoAno").val();
+        duracao = $("#campoDuracao").val();
+        // preparar dados no formato json
+        var dados = JSON.stringify({ nome: nome, artista: artista, genero: genero, ano: ano, duracao: duracao });
+        // fazer requisição para o back-end
+        $.ajax({
+            url: 'http://localhost:5000/incluir_musica',
+            type: 'POST',
+            dataType: 'json', // os dados são recebidos no formato json
+            contentType: 'application/json', // tipo dos dados enviados
+            data: dados, // estes são os dados enviados
+            success: musicaIncluida, // chama a função listar para processar o resultado
+            error: erroAoIncluir
+        });
+        function musicaIncluida (retorno) {
+            if (retorno.resultado == "ok") { // a operação deu certo?
+                // informar resultado de sucesso
+                alert("Música incluída com sucesso!");
+                // limpar os campos
+                $("#campoNome").val("");
+                $("#campoArtista").val("");
+                $("#campoGenero").val("");
+                $("#campoAno").val("");
+                $("#campoDuracao").val("");
+            } else {
+                // informar mensagem de erro
+                alert(retorno.resultado + ":" + retorno.detalhes);
+            }            
+        }
+        function erroAoIncluir (retorno) {
+            // informar mensagem de erro
+            alert("ERRO: "+retorno.resultado + ":" + retorno.detalhes);
+        }
+    });
 
+    // código a ser executado quando a janela de inclusão de pessoas for fechada
+    $('#modalIncluirMusica').on('hide.bs.modal', function (e) {
+        // se a página de listagem não estiver invisível
+        if (! $("#tabelaMusicas").hasClass('invisible')) {
+            // atualizar a página de listagem
+            exibir_musicas();
+        }
+    });
 
     // a função abaixo é executada quando a página abre
     mostrar_conteudo("conteudoInicial");
